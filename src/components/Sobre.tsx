8d76d689d500a8a638017bde9919df1dc746cdc8
Linel121 <pipipixaty08@gmail.com>
2025-02-19 13:20:35 -0500

    cmabioss

--- src/components/Sobre.tsx
+++ src/components/Sobre.tsx
@@ -8,7 +8,7 @@ const Sobre = () => {
       animate={{ opacity: 1, x: 0 }}
       transition={{ duration: 1 }}
       className="text-white font-semibold text-xl text-center 
-                 mt-[20px] sm:mt-[-350px] md:mt-[-350px] 
+                 mt-[-10px] sm:mt-[-500px] md:mt-[-350px] 
                  lg:mt-[-400px] xl:mt-[-400px] 
                  ml-0 sm:ml-[1rem] md:ml-[6rem] lg:ml-[10rem] xl:ml-[14rem]"
     >
